c54729706ce6d6d3b906e92d18b55667dbe61eb6
Samuel Abels <knipknap@gmail.com>
2011-01-28 15:29:56 +0100

    util.escape(): ignore non-ascii chars.

--- src/pyexist/util.py
+++ src/pyexist/util.py
@@ -8,7 +8,7 @@ def escape(arg):
     if hasattr(arg, '__iter__'):
         items = [("'" + escape(i) + "'") for i in arg]
         return ', '.join(items)
-    return str(arg).replace(r"'", r"''")
+    return arg.encode('ascii', 'ignore').replace(r"'", r"''")
 
 def replacetags(string, **kwargs):
     for key, value in kwargs.iteritems():
